1f872ea127a5f0eb3ae3d5e1f32bf93aed518cb4
Jose Cordova <josecordova@Joses-MacBook-Pro.local>
2020-09-21 20:29:46 -0600

    Project file structure and first endpoint

---
+++ src/controllers/health.controller.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import config from 'config';
+
+const port = config.get('port');
+
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+    return res.status(200).json({
+        server_up: true,
+        port: port,
+        environment: config.get('environment'),
+        lesson: 'ONE'
+    });
+});
+
+export default router;
